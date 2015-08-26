45cb8db0c23fc8ee7a67092949c9a820c74b33d5
Tyler <tyler.tanaka31@gmail.com>
2015-08-25 22:25:52 -0700

    First commit

---
+++ gulp/config.js
@@ -0,0 +1,27 @@
+var dest = "./build";
+var src = './src';
+
+module.exports = {
+  browserSync: {
+    server: {
+      // Serve up our build folder
+      baseDir: dest
+    }
+  },
+  sass: {
+    src: src + "/sass/**/*.{sass,scss}",
+    dest: dest + '/css',
+    settings: {
+      indentedSyntax: false, // Enable .sass syntax!
+      imagePath: 'images' // Used by the image-url helper
+    }
+  },
+  images: {
+    src: src + "/images/**",
+    dest: dest + "/images"
+  },
+  markup: {
+    src: src + "/htdocs/**",
+    dest: dest
+  },
+};
